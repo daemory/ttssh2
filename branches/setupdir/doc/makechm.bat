dd4912cd6f9bb38f40b1d6a26fc93f27e0dc73f4
zmatsuo <zmatsuo@f5f01b69-1e22-0410-acbf-894ab4bd6246>
2022-05-02 13:56:12 +0000

    setupdir listview

---
+++ branches/setupdir/doc/makechm.bat
@@ -0,0 +1,34 @@
+
+if exist "%Programfiles(x86)%\HTML Help Workshop\hhc.exe" (
+	set HELP_COMPILER="%Programfiles(x86)%\HTML Help Workshop\hhc.exe"
+) else (
+	set HELP_COMPILER=C:\progra~1\htmlhe~1\hhc.exe
+)
+
+REM for winxp installation on other drive or other language
+if exist "%Programfiles%\HTML Help Workshop\hhc.exe" (
+set HELP_COMPILER="%Programfiles%\HTML Help Workshop\hhc.exe"
+)
+
+set updated=
+
+CALL convtext.bat
+
+REM Check Japanese version Windows
+if not "%APPVEYOR%" == "True" (
+    chcp | find "932" > NUL
+    if ERRORLEVEL 1 goto English
+)
+
+for /f "delims=" %%i in ('perl htmlhelp_update_check.pl ja teratermj.chm') do @set updated=%%i
+if "%updated%"=="updated" (
+perl htmlhelp_index_make.pl ja html -o ja\Index.hhk
+%HELP_COMPILER% ja\teraterm.hhp
+)
+
+:English
+for /f "delims=" %%i in ('perl htmlhelp_update_check.pl en teraterm.chm') do @set updated=%%i
+if "%updated%"=="updated" (
+perl htmlhelp_index_make.pl en html -o en\Index.hhk
+%HELP_COMPILER% en\teraterm.hhp
+)
---
+++ branches/setupdir/libs/putty.cmake
@@ -0,0 +1,39 @@
+﻿# cmake -P putty.cmake
+
+set(SRC_DIR_BASE "putty-0.76")
+set(SRC_ARC "putty-0.76.tar.gz")
+set(SRC_URL "https://the.earth.li/~sgtatham/putty/0.76/putty-0.76.tar.gz")
+set(SRC_ARC_HASH_SHA256 547cd97a8daa87ef71037fab0773bceb54a8abccb2f825a49ef8eba5e045713f)
+
+set(DOWN_DIR "${CMAKE_SOURCE_DIR}/download/putty")
+set(EXTRACT_DIR "${CMAKE_SOURCE_DIR}/build/putty/src")
+set(SRC_DIR "${CMAKE_SOURCE_DIR}/build/putty/src/${SRC_DIR_BASE}")
+set(INSTALL_DIR "${CMAKE_SOURCE_DIR}/putty")
+
+if(NOT EXISTS ${INSTALL_DIR}/README)
+
+  file(DOWNLOAD
+    ${SRC_URL}
+    ${DOWN_DIR}/${SRC_ARC}
+    EXPECTED_HASH SHA256=${SRC_ARC_HASH_SHA256}
+    SHOW_PROGRESS
+    )
+
+  file(MAKE_DIRECTORY ${EXTRACT_DIR})
+
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar "xvf" ${DOWN_DIR}/${SRC_ARC}
+    WORKING_DIRECTORY ${EXTRACT_DIR}
+    )
+
+  file(REMOVE_RECURSE ${INSTALL_DIR})
+  file(RENAME ${SRC_DIR} ${INSTALL_DIR})
+  file(REMOVE_RECURSE ${EXTRACT_DIR})
+
+  file(COPY
+    ${INSTALL_DIR}/LICENCE
+    DESTINATION ${CMAKE_CURRENT_LIST_DIR}/doc_help)
+  file(RENAME
+    ${CMAKE_CURRENT_LIST_DIR}/doc_help/LICENCE
+    ${CMAKE_CURRENT_LIST_DIR}/doc_help/PuTTY-LICENSE.txt)
+endif()
